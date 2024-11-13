229c7561fa77d9fc509ce6432448328c7c6a7e87
Magezi Richard Elijah <magezirichardelijah@gmail.com>
2024-11-13 10:39:15 -0800

    Add files via upload

---
+++ page.js
@@ -0,0 +1,30 @@
+
+import Title from "../components/Title";
+import Content from "../components/Contents";
+
+function Home() {
+
+
+  return (
+    <div>
+      <Title heading="MAKERERE BLOG" />
+      <Content details="This is a very nice blog from MAK" />
+
+      <br />
+
+      <Title heading="UGANDA MARTYRS BLOG" />
+      <Content details="This is a very nice blog from UMU" />
+      <br />
+
+      <Title heading="NDEJJE BLOG" />
+      <Content details="This is a very nice blog from NDEJJE" />
+             
+            
+
+
+    </div>
+  );
+}
+
+export default Home;
+
